6570a62bdc115d56b0d44fcb8283b5ac7ba30550
Guilherme Humberto <gbreu@outlook.com>
2020-08-29 21:48:01 -0300

    Routas

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+// Criação das rotas da aplicação
+
+import React from 'react';
+import { Switch, Route} from 'react-router-dom'
+
+// import Component from '../pages/'
+
+function Routes() {
+  return (
+      <Switch>
+          <Route path="/" component={} />
+      </Switch>
+  );
+}
+
+export default Routes;
