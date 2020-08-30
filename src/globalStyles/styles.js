652bf4f6afa06eb97e1605777528827827ce13c7
Guilherme Humberto <gbreu@outlook.com>
2020-08-29 21:47:13 -0300

    Estilos globais

---
+++ src/globalStyles/styles.js
@@ -0,0 +1,13 @@
+// Definindo estilos globais
+
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+ * {
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+ }
+
+`
