c5e9731fb49d86be312603fe7154d0a0f88274ed
Guilherme Humberto <gbreu@outlook.com>
2020-08-29 21:49:10 -0300

    Arquivo principal render

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './globalStyles/styles'
+import Routes from './routes/routes';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
